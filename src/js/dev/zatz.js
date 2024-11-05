4e854a220d7ca25fb6ec7166293b5bb81a786a3a
zatzoid <zatzoid@gmail.com>
2024-11-05 18:23:39 +0300

    sections

--- src/js/dev/zatz.js
+++ src/js/dev/zatz.js
@@ -2,13 +2,16 @@ import $ from 'jquery'
 import Form from '../utils/Form'
 import Inputmask from 'inputmask'
 import Swiper from 'swiper';
-import { Navigation, Pagination, Grid, Autoplay } from 'swiper/modules';
-
+import { Navigation, Pagination, Grid, Autoplay, EffectFade } from 'swiper/modules';
+import { rem } from '../utils/constants'
+import { Fancybox } from "@fancyapps/ui";
 
 $(function () {
     dropDowns()
     initSwitchList()
-
+    initSwipers()
+    initFancybox()
+    reviewOpenReview()
 })
 
 function initForms() {
@@ -40,33 +43,154 @@ function dropDowns() {
 
 }
 function initSwipers() {
-    /* const complects = document.querySelectorAll('.complects__c-slider')
-    if (complects) {
-        complects.forEach((e) => {
+    const mobile = document.querySelectorAll('.mobile-slider.swiper')
+    if (mobile) {
+        mobile.forEach((e) => {
             new Swiper(e, {
                 modules: [Navigation, Pagination],
                 loop: false,
-                slidesPerView: 1,
-                slidesPerGroup: 1,
-                spaceBetween: rem(3),
+                slidesPerView: 1.05,
+                spaceBetween: rem(1),
                 breakpoints: {
                     768: {
-                        slidesPerView: 5,
-                        slidesPerGroup: 1,
-
+                        slidesPerView: e.dataset.desktopSlides,
+                        spaceBetween: rem(3.2),
                     }
                 },
                 pagination: {
-                    el: e.querySelector('.complects__c-slider-pagination')
+                    el: e.querySelector('.swiper-pagination')
                 },
                 navigation: {
-                    prevEl: e.querySelector('.complects__c-slider-prev'),
-                    nextEl: e.querySelector('.complects__c-slider-next')
+                    prevEl: e.querySelector('.swiper-btn-prev'),
+                    nextEl: e.querySelector('.swiper-btn-next')
                 },
 
             })
         })
-    } */
+    }
+
+    const prices = document.querySelectorAll('.prices-list');
+    if (prices) {
+        prices.forEach((e) => {
+            new Swiper(e.querySelector('.swiper'), {
+                modules: [Navigation, Pagination, Grid],
+                loop: false,
+                slidesPerView: 1.05,
+                spaceBetween: rem(1),
+                breakpoints: {
+                    768: {
+                        slidesPerView: 2,
+                        slidesPerGroup: 4,
+                        spaceBetween: rem(3.2),
+                        grid: {
+                            fill: 'row',
+                            rows: Math.ceil(Array.from(e.querySelectorAll('.swiper-slide')).length / 2)
+                        },
+                    }
+                },
+
+                pagination: {
+                    el: e.querySelector('.swiper-pagination')
+                },
+                navigation: {
+                    prevEl: e.querySelector('.swiper-btn-prev'),
+                    nextEl: e.querySelector('.swiper-btn-next')
+                },
+
+            })
+        })
+    }
+
+    const defaultSlider = document.querySelectorAll('.default-swiper')
+    if (defaultSlider) {
+        defaultSlider.forEach((e) => {
+            new Swiper(e.querySelector('.swiper'), {
+                modules: [Navigation, Pagination],
+                loop: false,
+                slidesPerView: 1.05,
+                spaceBetween: rem(1),
+                breakpoints: {
+                    768: {
+                        slidesPerView: e.dataset.desktopview ? e.dataset.desktopview : 4,
+                        spaceBetween: rem(3.2),
+                    }
+                },
+                pagination: {
+                    el: e.querySelector('.swiper-pagination')
+                },
+                navigation: {
+                    prevEl: e.querySelector('.swiper-btn-prev'),
+                    nextEl: e.querySelector('.swiper-btn-next')
+                },
+
+            })
+        })
+    }
+
+    const placement = document.querySelector('.placement')
+    if (placement) {
+        const images = new Swiper(placement.querySelector('.placement__c-slider-img'), {
+            modules: [Navigation, Pagination],
+            loop: false,
+            slidesPerView: 1,
+            simulateTouch: false
+
+        })
+
+        new Swiper(placement.querySelector('.placement__c-slider-body'), {
+            modules: [Navigation, Pagination],
+            loop: false,
+            slidesPerView: 1,
+            navigation: {
+                prevEl: placement.querySelector('.placement__c-slider-body').querySelector('.swiper-btn-prev'),
+                nextEl: placement.querySelector('.placement__c-slider-body').querySelector('.swiper-btn-next')
+            },
+            on: {
+                slideChange: (swiper) => {
+                    images.slideTo(swiper.activeIndex)
+                }
+            }
+
+        })
+
+    }
+
+    const clinicPhotos = document.querySelectorAll('.clinic-photo');
+    if (clinicPhotos) {
+        clinicPhotos.forEach((e) => {
+            new Swiper(e.querySelector('.swiper'), {
+                modules: [Navigation, Pagination, Grid],
+                loop: false,
+                spaceBetween: rem(1),
+                slidesPerView: 2.1,
+                slidesPerGroup: 2,
+                grid: {
+                    fill: 'row',
+                    rows: 2
+                },
+                breakpoints: {
+                    768: {
+                        spaceBetween: rem(3.2),
+                        slidesPerView: 5,
+                        slidesPerGroup: 1,
+                        grid: {
+                            fill: 'row',
+                            rows: 1
+                        },
+                    }
+                },
+
+                pagination: {
+                    el: e.querySelector('.swiper-pagination')
+                },
+                navigation: {
+                    prevEl: e.querySelector('.swiper-btn-prev'),
+                    nextEl: e.querySelector('.swiper-btn-next')
+                },
+
+            })
+        })
+    }
 
 }
 function modalsHandler() {
@@ -136,4 +260,69 @@ function initSwichers() {
       if (basketDelivery) {
           new Switcher(basketDelivery, 0)
       } */
+}
+
+function initFancybox() {
+    const anytarget = document.querySelector('[data-fancybox]')
+    if (!anytarget) return
+
+
+
+    Fancybox.bind('[data-fancybox]', {
+        Thumbs: false,
+        Toolbar: {
+            display: {
+                left: [],
+                middle: ["infobar"],
+                right: ["close"],
+            },
+        },
+    })
+}
+
+function reviewOpenReview() {
+    if (!document.querySelector('.review-slide__body-text')) return
+
+    const container = $('.review-slide'),
+        textSelector = '.review-slide__body-text',
+        text = $(textSelector).toArray(),
+        textContainerSelector = '.review-slide__body',
+        shortenedTextSelecor = 'review-slide__body-short'
+
+    const maxHeight = 300
+    text.forEach((e) => {
+        if (e.offsetHeight > maxHeight) {
+            e = $(e)
+            e.addClass(shortenedTextSelecor)
+            e.closest(textContainerSelector)
+                .append('<button class="btn-underline col-bl txt18 ">развернуть</button>')
+        }
+
+    })
+
+    container.on('click', (ev) => {
+        if (!ev.target.classList.contains('btn-underline')) return
+
+        if (!ev.target.closest(textContainerSelector).classList.contains('_opened')) {
+            const parent = ev.target.closest(textContainerSelector)
+            parent.classList.add('_opened')
+            parent.querySelector(textSelector)
+                .classList.remove(shortenedTextSelecor)
+            ev.target.textContent = 'свернуть'
+
+
+        } else {
+            const parent = ev.target.closest(textContainerSelector)
+            parent.classList.remove('_opened')
+            parent.querySelector(textSelector)
+                .classList.add(shortenedTextSelecor)
+            ev.target.textContent = 'развернуть'
+        }
+
+
+
+    })
+
+
+
 }
